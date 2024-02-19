f1bbf5b267d17856e67174c0c34936fcbf305378
The-real-joe <j.boucher690@gmail.com>
2024-02-19 15:32:10 -0500

    finished

--- api/server.js
+++ api/server.js
@@ -1,106 +1,142 @@
-const express = require('express');
+const express = require('express')
 const User = require('./users/model')
-const { v4: uuid } = require('uuid');
-// creating object
+
 const server = express();
 
-// telling server to parse the JSON in body of request
 server.use(express.json());
 
-// request handlers
-// get handlers
+
+//GET USERS
 server.get('/api/users', (req, res) => {
     User.find()
-        .then((result) => {
-            res.status(200).json(result);
+        .then(users => {
+            res.json(users)
         })
-        .catch((err) => {
+        .catch( err => {
             res.status(500).json({
-                message: 'The users information could not be retrieved'
-            })
-    })
-})
-
-server.get('/api/users/:id', (req, res) => {
-    User.findById(req.params.id)
-        .then((result) => {
-            if (!result) {
-                res.status(404).json({
-                    message: "The user with the specified ID does not exist"
-                })
-            }
-            else res.status(200).json(result)
-        })
-        .catch((err) => {
-            res.status(500).json({
-                message: 'cannot get user',
+                message: 'error getting users',
                 err: err.message
-        })
-    })
-})
-
-// post handlers
-server.post('/api/users', (req, res) => {
-    User.insert(req.body)
-        .then((result) => {
-            if (!req.body.name || !req.body.bio) {
-                res.status(400).json({
-                    message: "Please provide name and bio for the user"
-                })
-            }
-            else res.status(201).json(result);
-        })
-        .catch((err) => {
-            res.status(500).json({
-                message: "There was an error while saving the user to the database"
             })
         })
     
 })
 
-// delete handlers
-server.delete('/api/users/:id', (req, res) => {
-    User.remove(req.params.id)
-        .then((result) => {
-            if (!result) {
-                res.status(404).json({
-                    message: "The user with the specified ID does not exist"
-                });
-            }
-            else res.status(200).json(result);
-        })
-        .catch((err) => {
-            res.status(500).json({
-                message: "The user could not be removed"
-            })
-        })  
-})
+//WILDCARD * CATCHALL FOR ENDPOINTS
 
-// put handlers
-server.put('/api/users/:id', (req, res) => {
-    User.update(req.params.id, {
-        name: req.body.name,
-        bio: req.body.bio
+server.use('*', (req, res) => {
+
+    res.status(404).json({
+        message: 'not found'
     })
-        .then((result) => {
-            if (!req.body.name || !req.body.bio) {
-                res.status(400).json({
-                    message: "Please provide name and bio for the user"
-                })
-            }
-            else if (!result) {
-                res.status(404).json({
-                    message: "The user with the specified ID does not exist"
-                })
-            }
-            else res.status(200).json(result)
+});
+
+
+//POST A NEW USER
+
+server.post('/api/users', (req, res) => {
+    const user = req.body
+    if (!user.name || !user.bio) {
+        res.status(400).json({
+            message: "Please provide name and bio for the user"})
+    } else {
+    User.insert(user)
+        .then(createdUser => {
+            res.status(201).json(createdUser)
         })
-        .catch((err) => {
+            .catch( err => {
             res.status(500).json({
-                message: 'The user information could not be modified'
+                message: "There was an error while saving the user to the database",
+                err: err.message
             })
         })
+         
+    }    
 })
 
-server.listen(8000, () => {console.log('api running')})
+//GET USER BY ID
+
+server.get('/api/users/:id', (req, res) => {
+    User.findById(req.params.id)
+    .then(user => {
+        if(!user) {
+            res.status(404).json({message: "The user with the specified ID does not exist"})
+        }
+        res.json(user)
+    })
+    .catch( err => {
+        res.status(500).json({
+            message: "The users information could not be retrieved",
+            err: err.message
+        })
+    })
+    
+})
+
+
+
+server.delete('/api/usesrs/:id', async (req, res) => {
+   try {
+   
+    const possibleUser = await User.findById(req.params.id)
+   
+    if(!possibleUser) {
+        res.status(404).json({
+            message: "The user with the specified ID does not exist" 
+            })
+    } else {
+        // same as         const deletedUser = await User.remove(req.params.id)
+
+        const deletedUser = await User.remove(possibleUser.id)
+        res.status(200).json(deletedUser)
+    }
+} catch (err) {
+    res.status(500).json({
+        message: "The users information could not be retrieved",
+        err: err.message
+    })
+}
+            
+       
+})
+
+
+//Updating a user
+
+server.put('/api/usesrs/:id', async (req, res) => {
+    try {
+        //verifying that the userid matches using the findbyid method set in the model module
+    const possibleUser = await User.findById(req.params.id)
+        if(!possibleUser) {
+            res.status(404).json({
+                message: "The user with the specified ID does not exist" 
+                })
+        } else {
+            if (!req.body.name || !req.body.bio) {
+            res.status(400).json({
+              message:  "Please provide name and bio for the user" 
+            })
+        } else {
+         const updatedUser = await  User.update(
+             req.params.id, 
+             req.body
+         ) 
+            res.status(200).json(updatedUser)
+            
+        }
+    }
+
+
+    }
+    catch (err) {
+        res.status(500).json({
+            message: "The user information could not be modified",
+            err: err.message
+        })
+    }
+})
+
+server.listen(9000, () => {
+    console.log('server running on port 9000')
+})
+
 module.exports = server; // EXPORT YOUR SERVER instead of {}
