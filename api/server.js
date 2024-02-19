78bc3cb0aad85e9d6bb4a8bf45e67c8a5cc4ef2e
The-real-joe <j.boucher690@gmail.com>
2024-02-19 15:23:41 -0500

    maybe

--- api/server.js
+++ api/server.js
@@ -1,3 +1,41 @@
 // BUILD YOUR SERVER HERE
+const express = require('express');
+const User = require('./users/model')
+const { v4: uuid } = require('uuid');
+// creating object
+const server = express();
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+// telling server to parse the JSON in body of request
+server.use(express.json());
+
+
+server.post('/api/users', (req, res) => {
+    const { name, bio } = req.body;
+    const user = { name, bio };
+    res.status(201).send(user);
+});
+
+server.get('/api/users/:id', (req, res) => {
+    const { id } = req.params;
+    const user = { id, name: 'John', bio: 'Developer' };
+    res.send(user);
+});
+
+server.get('/api/users', (req, res) => {
+    const users = [{ id: 1, name: 'John', bio: 'Developer' }];
+    res.send(users);
+});
+
+server.put('/api/users/:id', (req, res) => {
+    const { id } = req.params;
+    const { name, bio } = req.body;
+    const user = { id, name, bio };
+    res.send(user);
+});
+
+server.delete('/api/users/:id', (req, res) => {
+    const { id } = req.params;
+    const user = { id, name: 'John', bio: 'Developer' };
+    res.send(user);
+});
+
--- index.js
+++ index.js
@@ -4,36 +4,6 @@ const port = 9000;
 
 // START YOUR SERVER HERE
 
-server.post('/api/users', (req, res) => {
-    const { name, bio } = req.body;
-    const user = { name, bio };
-    res.status(201).send(user);
-});
-
-server.get('/api/users/:id', (req, res) => {
-    const { id } = req.params;
-    const user = { id, name: 'John', bio: 'Developer' };
-    res.send(user);
-});
-
-server.get('/api/users', (req, res) => {
-    const users = [{ id: 1, name: 'John', bio: 'Developer' }];
-    res.send(users);
-});
-
-server.put('/api/users/:id', (req, res) => {
-    const { id } = req.params;
-    const { name, bio } = req.body;
-    const user = { id, name, bio };
-    res.send(user);
-});
-
-server.delete('/api/users/:id', (req, res) => {
-    const { id } = req.params;
-    const user = { id, name: 'John', bio: 'Developer' };
-    res.send(user);
-});
-
 
 
 
