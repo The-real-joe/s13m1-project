5a85c737213043ecacdffc4f96fe962529fc85d1
The-real-joe <j.boucher690@gmail.com>
2024-02-19 15:37:18 -0500

    maybe!

--- api/server.js
+++ api/server.js
@@ -1,142 +1,118 @@
 const express = require('express')
 const User = require('./users/model')
-
-const server = express();
-
-server.use(express.json());
+const server = express()
+server.use(express.json())
 
 
-//GET USERS
-server.get('/api/users', (req, res) => {
-    User.find()
-        .then(users => {
-            res.json(users)
-        })
-        .catch( err => {
-            res.status(500).json({
-                message: 'error getting users',
-                err: err.message
+server.put('/api/users/:id', async (req, res) => {
+    try {
+        const possibleUser = await User.findById(req.params.id)
+        if(!possibleUser) {
+            res.status(404).json({
+                message: "The user with the specified ID does not exist" 
             })
+        } else {
+            if (!req.body.name || !req.body.bio) {
+                res.status(400).json({
+                    message: 'Please provide name and bio for the user'
+                })
+            } else {
+                const updatedUser = await User.update(
+                    req.params.id, 
+                    req.body
+                    )
+                res.status(200).json(updatedUser)
+            }
+        }
+    } catch (err) {
+        res.status(500).json({
+            message: 'error updating user',
+            err: err.message, 
+            stack: err.stack
         })
-    
+    }
 })
 
-//WILDCARD * CATCHALL FOR ENDPOINTS
+server.delete('/api/users/:id', async (req, res) => {
+    try {
+    const possibleUser = await User.findById(req.params.id)
+    if(!possibleUser) {
+        res.status(404).json({
+            message: "The user with the specified ID does not exist" 
+        })
+    } else {
+        const deletedUser = await User.remove(possibleUser.id)
+        res.status(200).json(deletedUser)
+    }
+    } catch (err) {
+        res.status(500).json({
+            message: 'error deleting user',
+            err: err.message, 
+            stack: err.stack
+        })
+    }
+})
 
-server.use('*', (req, res) => {
 
-    res.status(404).json({
-        message: 'not found'
-    })
-});
-
-
-//POST A NEW USER
 
 server.post('/api/users', (req, res) => {
-    const user = req.body
+    const user = req.body;
     if (!user.name || !user.bio) {
-        res.status(400).json({
-            message: "Please provide name and bio for the user"})
+        res.status(400).json({ 
+            message: "Please provide name and bio for the user",
+        })
     } else {
-    User.insert(user)
+        User.insert(user)
         .then(createdUser => {
             res.status(201).json(createdUser)
         })
-            .catch( err => {
+        .catch(err => {
             res.status(500).json({
-                message: "There was an error while saving the user to the database",
-                err: err.message
+                message: 'error creating user',
+                err: err.message, 
+                stack: err.stack
             })
         })
-         
-    }    
+    }
 })
 
-//GET USER BY ID
+server.get('/api/users', (req, res) => {
+    User.find()
+    .then(users => {
+        res.json(users)
+    })
+    .catch(err => {
+        res.status(500).json({
+            message: 'error getting users',
+            err: err.message, 
+            stack: err.stack
+        })
+    })
+})
 
 server.get('/api/users/:id', (req, res) => {
     User.findById(req.params.id)
     .then(user => {
         if(!user) {
-            res.status(404).json({message: "The user with the specified ID does not exist"})
+            res.status(404).json({ 
+                message: "The user with the specified ID does not exist" 
+        })
         }
         res.json(user)
     })
-    .catch( err => {
+    .catch(err => {
         res.status(500).json({
-            message: "The users information could not be retrieved",
-            err: err.message
+            message: 'error getting user',
+            err: err.message, 
+            stack: err.stack
         })
     })
-    
 })
 
-
-
-server.delete('/api/usesrs/:id', async (req, res) => {
-   try {
-   
-    const possibleUser = await User.findById(req.params.id)
-   
-    if(!possibleUser) {
-        res.status(404).json({
-            message: "The user with the specified ID does not exist" 
-            })
-    } else {
-        // same as         const deletedUser = await User.remove(req.params.id)
-
-        const deletedUser = await User.remove(possibleUser.id)
-        res.status(200).json(deletedUser)
-    }
-} catch (err) {
-    res.status(500).json({
-        message: "The users information could not be retrieved",
-        err: err.message
+server.use('*', (req, res) => {
+    res.status(404).json({
+        message: 'not found'
     })
-}
-            
-       
 })
 
-
-//Updating a user
-
-server.put('/api/usesrs/:id', async (req, res) => {
-    try {
-        //verifying that the userid matches using the findbyid method set in the model module
-    const possibleUser = await User.findById(req.params.id)
-        if(!possibleUser) {
-            res.status(404).json({
-                message: "The user with the specified ID does not exist" 
-                })
-        } else {
-            if (!req.body.name || !req.body.bio) {
-            res.status(400).json({
-              message:  "Please provide name and bio for the user" 
-            })
-        } else {
-         const updatedUser = await  User.update(
-             req.params.id, 
-             req.body
-         ) 
-            res.status(200).json(updatedUser)
-            
-        }
-    }
-
-
-    }
-    catch (err) {
-        res.status(500).json({
-            message: "The user information could not be modified",
-            err: err.message
-        })
-    }
-})
-
-server.listen(9000, () => {
-    console.log('server running on port 9000')
-})
-
-module.exports = server; // EXPORT YOUR SERVER instead of {}
+module.exports = server; 
